1d3679e6041cb697142883938659146572a46e36
FranciscoP <pakito15191@hotmail.com>
2013-01-08 17:40:22 +0100

    Update class.transladb.php

--- class.transladb.php
+++ class.transladb.php
@@ -1,4 +1,5 @@
 <?php
+<?php
 /* 
 * Transladb
 * @Version  0.8
@@ -9,47 +10,45 @@ class Translate
   private $Lang;
   private $DB;
   
-  /* 
-  *  Pass the right parameters. Reason for passing $DB instead of extending the $DB class here: http://stackoverflow.com/q/7827713/938236
-  */
+  /* Pass the right parameters or you will burn in hell until the end of time. */
   public function __construct ($DB, $Lang)
     {
     $this->DB = $DB;
     $this->Lang = $Lang;
     }
 
-  /*
-  *  Simplifies the calling method to $_("keyword");
-  *  How it works: http://stackoverflow.com/q/13221863/938236
-  */
-  public function __invoke( $keyword, $Arg = null )
+  /* Requires PHP 5.3+. Simplifies the method calling to $_("keyword"); */
+  public function __invoke($Id, $Arg = null)
     {
-    $Text = $this->retrieve( $keyword );
-    if (empty($Text))
+    $Id = str_replace(" ", "_", $Id);
+    if (!($Text = $this->retrieve($Id)))
       {
-      $Text = str_replace("_", " ", $keyword);  /* If not found, replace all "_" with " " from the input string. */
-      $this->add($keyword, $Text);              /* Adds the new strings dynamically so it doesn't break the flow. */
+      /* Adds the string dynamically to database so it doesn't break the flow. */
+      $Text = str_replace("_", " ", $Id);
+      $this->add($Id, $Text);
       }
-    return str_replace("%s", $Arg, $Text);      /* Not likely to have 2 or more variables into a single string. */
+    return str_replace("%s", $Arg, $Text);    // Not likely to have more than 2 variables into a single string.
     }
   
-  /* It returns the right cell, however it might be empty. No default language option on purpose. */
-  private function retrieve ( $Id )
-    {
-    $STH = $this->DB->prepare("SELECT * FROM translations WHERE keyword=? LIMIT 1");
-    $STH->execute(array($Id));
-    $row = $STH->fetch();
-    return $row[$this->Lang];
-    }
-  
-  /* If the string doesn't exist it creates it. Useful not to break the coding flow. */
-  private function add ( $Id, $Text )
+  /* If the string doesn't exist it creates it. Useful not to break the flow. */
+  private function add ($Id, $Text)
     {
     $STH = $this->DB->prepare("INSERT INTO translations (keyword, en, last) VALUES (?, ?, now())");
     $STH->execute(array($Id,$Text));
+    }
+  
+  /* It returns the right cell, however it might be empty. No default language option. */
+  private function retrieve ($Id)
+    {
+    $STH = $this->DB->prepare("SELECT * FROM translations WHERE keyword=? LIMIT 1");
+    $STH->execute(array($Id));
+      
+    $row = $STH->fetch();
+    return $row[$this->Lang];
     }
   }
 
 /* Create the object. You might want to change this line somewhere else. */
 $_ = new Translate;
+echo $_("This is a test to show how %s works", "TranslateDB");
 ?>
