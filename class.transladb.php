196c70197e45ed1c6682d61938647b3207f1c029
FranciscoP <pakito15191@hotmail.com>
2013-01-18 03:31:13 +0100

    Update class.transladb.php

--- class.transladb.php
+++ class.transladb.php
@@ -1,19 +1,19 @@
 <?php
-/* 
+/*
 * Transladb
-* @License  GNU GPL 3+
+* @Version  0.8
 * @Author   Francisco Presencia Fandos
 */
 class Translate
   {
-  private $Lang;
+  private $Language;
   private $DB;
   
-  /* Set the needed values in the initialization. */
-  public function __construct ($DB, $Lang)
+  /* Pass the right parameters or you will burn in hell until the end of time. */
+  public function __construct ($DB, $Language)
     {
     $this->DB = $DB;
-    $this->Lang = $Lang;
+    $this->Language = $Language;
     }
 
   /* Requires PHP 5.3+. Simplifies the method calling to $_("keyword"); */
@@ -29,11 +29,16 @@ class Translate
     return str_replace("%s", $Arg, $Text);    // Not likely to have more than 2 variables into a single string.
     }
   
-  /* If the string doesn't exist it creates it. Useful not to break the coding flow. */
-  private function add ($Id, $Text)
+  /* If the string doesn't exist it creates it. Useful not to break the flow. */
+  public function add ($Id, $Text)
     {
-    $STH = $this->DB->prepare("INSERT INTO translations (keyword, en, last) VALUES (?, ?, now())");
-    $STH->execute(array($Id,$Text));
+    $q = $this->DB->query("DESCRIBE translations");
+    if (in_array ($this->Language, $q->fetchAll(PDO::FETCH_COLUMN)));
+      {
+      $STH = $this->DB->prepare("INSERT INTO translations (keyword, ".$this->Language.", last) VALUES (?, ?, now())
+                                  ON DUPLICATE KEY UPDATE ".$this->Language." = ?");
+      return $STH->execute(array($Id, $Text, $Text));
+      }
     }
   
   /* It returns the right cell, however it might be empty. No default language option. */
@@ -42,14 +47,8 @@ class Translate
     $STH = $this->DB->prepare("SELECT * FROM translations WHERE keyword=? LIMIT 1");
     $STH->execute(array($Id));
       
+    /* If you have PHP 5.4+, you could do: "return $STH->fetch()[$this->Lang];" instead of next lines. Less memory and work involved. */
     $row = $STH->fetch();
-    return $row[$this->Lang];
+    return $row[$this->Language];
     }
   }
-
-/* Create the object. You might want to change this line somewhere else. Will automatically throw error if not set. */
-$_ = new Translate ($DB, $Language);
-
-/* Testing the object. It should display "This is a test to show how TranslateDB works" */
-echo $_("This is a test to show how %s works", "TranslateDB");
-?>
