f55053cc22716c5dd71116a82ca2fffa4aad21dd
FranciscoP <pakito15191@hotmail.com>
2013-02-09 15:30:16 +0100

    Update class.transladb.php

--- class.transladb.php
+++ class.transladb.php
@@ -16,29 +16,27 @@ class Translate
     $this->Language = $Language;
     }
 
-  /* Requires PHP 5.3+. Simplifies the method calling to $_("keyword"); */
-  public function __invoke($Id, $Arg = null)
+  /* Requires PHP 5.3+. Simplifies the calling method to echo $_('keyword'); */
+  public function __invoke($Key, $Arg = null)
     {
-    $Id = str_replace(" ", "_", $Id);
+    $Id = substr($Key, 0, 64);
+    /* If there's no text string yet */
     if (!($Text = $this->retrieve($Id)))
       {
-      /* Adds the string dynamically to database so it doesn't break the flow. */
-      $Text = str_replace("_", " ", $Id);
-      $this->add($Id, $Text);
+      /* Add the string dynamically to database so it doesn't break the coding flow. */
+      $Text = $this->add($Id, $Key);
       }
-    return str_replace("%s", $Arg, $Text);    // Not likely to have more than 2 variables into a single string.
+    /* Not likely to have 2 or more variables into a single string. */
+    return str_replace("%s", $Arg, $Text);
     }
   
-  /* If the string doesn't exist it creates it. Useful not to break the flow. */
+  /* Adds to database a new translation row in English. */
   public function add ($Id, $Text)
     {
-    $q = $this->DB->query("DESCRIBE translations");
-    if (in_array ($this->Language, $q->fetchAll(PDO::FETCH_COLUMN)));
-      {
-      $STH = $this->DB->prepare("INSERT INTO translations (keyword, ".$this->Language.", last) VALUES (?, ?, now())
-                                  ON DUPLICATE KEY UPDATE ".$this->Language." = ?");
-      return $STH->execute(array($Id, $Text, $Text));
-      }
+    $STH = $this->DB->prepare("INSERT INTO translations (keyword, en, last) VALUES (?, ?, now())
+                                ON DUPLICATE KEY UPDATE en = ?");
+    $STH->execute(array($Id, $Text, $Text));
+    return $Text;
     }
   
   /* It returns the right cell, however it might be empty. No default language option. */
@@ -47,7 +45,7 @@ class Translate
     $STH = $this->DB->prepare("SELECT * FROM translations WHERE keyword=? LIMIT 1");
     $STH->execute(array($Id));
       
-    /* If you have PHP 5.4+, you could do: "return $STH->fetch()[$this->Lang];" instead of next lines. Less memory and work involved. */
+    /* If you have PHP 5.4+, you could do: "return $STH->fetch()[$this->Lang];" instead of next lines. */
     $row = $STH->fetch();
     return $row[$this->Language];
     }
