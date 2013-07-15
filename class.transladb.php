fa5d157cd09b4bfea6655075c5cc1d1120d1022c
FranciscoP <pakito15191@hotmail.com>
2013-07-15 19:46:34 +0200

    Update class.transladb.php
    You don't want to overwrite the old sentence which might be already translated. Also, a production/development variable added.

--- class.transladb.php
+++ class.transladb.php
@@ -6,45 +6,47 @@
 */
 class Translate
   {
+  // Automatically add the word if it doesn't exist yet. Recommended: deactivate on production.
+  private $AutoAdd = 1;
+  
   private $Language;
   private $DB;
   
-  /* Pass the right parameters or you will burn in hell until the end of time. */
-  public function __construct ($DB, $Language)
+  public function __construct (PDO $DB, $Language)
     {
     $this->DB = $DB;
     $this->Language = $Language;
     }
 
-  /* Requires PHP 5.3+. Simplifies the calling method to echo $_('keyword'); */
+  // Requires PHP 5.3+. Simplifies the calling method to echo $_('keyword');
   public function __invoke($Key)
     {
     $Id = substr($Key, 0, 64);
-    /* If there's no text string yet */
-    if (!($Text = $this->retrieve($Id)))
+    // If there's no text string yet
+    if (!($Text = $this->retrieve($Id)) && $AutoAdd)
       {
-      /* Add the string dynamically to database so it doesn't break the coding flow. */
+      // Add the string dynamically to database so it doesn't break the coding flow.
       $Text = $this->add($Id, $Key);
       }
     return $Text;
     }
   
-  /* Adds to database a new translation row in English. */
+  // Adds to database a new translation row in English. This method is disabled in production (and can be deleted).
   public function add ($Id, $Text)
     {
-    $STH = $this->DB->prepare("INSERT INTO translations (keyword, en) VALUES (?, ?)
-                                ON DUPLICATE KEY UPDATE en = ?");
+    // Ignore not to overwrite the old translations.
+    $STH = $this->DB->prepare("INSERT IGNORE INTO translate (keyword, en) VALUES (?, ?)");
     $STH->execute(array($Id, $Text, $Text));
     return $Text;
     }
   
-  /* It returns the right cell, however it might be empty. No default language option. */
+  // It returns the right cell, however it might be empty. No default language option.
   private function retrieve ($Id)
     {
     $STH = $this->DB->prepare("SELECT * FROM translations WHERE keyword=? LIMIT 1");
     $STH->execute(array($Id));
       
-    /* If you have PHP 5.4+, you could do: "return $STH->fetch()[$this->Lang];" instead of next lines. */
+    // If you have PHP 5.4+, you could do: "return $STH->fetch()[$this->Lang];" instead of next lines.
     $row = $STH->fetch();
     return $row[$this->Language];
     }
